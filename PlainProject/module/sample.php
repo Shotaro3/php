60fd00bd330b12be265cc7cc4cc9e54b25517fb8
shotaro <shotaro@KannoShotaro.mshome.net>
2015-03-13 02:15:47 +0900

    ざっくり（だけど少し）書いただけ
    定数をちまちま作る感じで
    
    リクエストでインスタンスを作成し、バインドする変数名(キー)があれば実行する感じにしたい
    でもこれで踏ん切りつけられない
    というのも上の処理が気になるから
    でもそのあたり考え出すとますます進まないのでモヤっとしてる
    
    明日は定数を作ってもう少し掘り下げたい

--- PlainProject/module/sample.php
+++ PlainProject/module/sample.php
@@ -1,10 +1,3 @@
-<?php
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', '1');
-
-// require_once (dirname(__FILE__).'/module/db.php');
-require_once (dirname(__FILE__).'/module/know.php');
-exit;
 //処理深さ　コントローラー　ステップ構造の管理のみ
 abstract class controler {
 	//
@@ -168,53 +161,3 @@ class hiragana extends controler {
 }
 
 new PlainSystem();
-exit;
-// session_start();
-
-//  セッション系
-//define(SESSION_SAVE_PATH, session_save_path());
-
-//var_dump(SESSION_SAVE_PATH);
-
-//$result = sprintf($SENTENCE['CREATE']['DB'], 'NewDB', '', '', '');
-
-//var_dump($result);
-
-/*
-DB接続情報です
- */
-define('DB_PATH', 'localhost');
-define('DB_NAME', 'sample');
-define('DB_USER', 'root');
-define('DB_PSWD', 'root');
-
-$db = new MySQL();
-$db->set_property(DB_PATH, DB_USER, DB_PSWD, DB_NAME);
-$db->open();
-$mysql_text['database'] = $db->put_query('SHOW DATABASES');
-
-if (isset($_POST['database'])) {
-	$db->change_use_database($_POST['database']);
-	$db->open();
-	$mysql_text['table'] = $db->put_query('SHOW TABLES');
-
-}
-
-// // URL含みを管理したい時に使う
-// var_dump($_GET);
-// var_dump($_REQUEST);
-
-// and ...true or String only
-
-//'a' and  or var_dump('<br>trueだよ<br>');
-
-// $ret;
-// $com = $_POST['command'];
-
-// mkdir ls
-//excec() ;
-// var_dump(exec($com, $ret).'<br>');
-
-// var_dump($ret);
-
-require_once (dirname(__FILE__).'/plane.html');
