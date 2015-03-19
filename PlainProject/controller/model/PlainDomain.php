14dd117731d514dcc427d01575d2a51fcfdc100b
shotaro <shotaro@KannoShotaro.mshome.net>
2015-03-20 00:50:48 +0900

    とり

--- PlainProject/controller/model/PlainDomain.php
+++ PlainProject/controller/model/PlainDomain.php
@@ -1,38 +1,29 @@
 <?php
-class  extends CommonController {
+final class Next extends CommonController {
+	const LAYER = DOMAIN;
+	const ROLE  = 'Next';
 
 	// notification this model code
-	const READ = 1;
-	const CREATE = 2;
-	const UPDATA = 3;
-	const DELETE = 4;
-
-	public $strage;
-	public $report;
+	const NEXT = 'NEXT';
+	const PREV = 'PREV';
+	// const RETRY = 'RETRY';
+	// const RESTART = 'RESTART';
 
 	public function __construct() {
-		$this->report = parent::__construct($this);
+		$object       = new body();
+		$this->report = parent::__construct($object);
 	}
-	// overhead processing
 	public function step01() {
-		new LoginControle($this->blankForm(LOGIN_CHECK, ALL_STEP));
 	}
-	// proper noun process
 	public function step02() {
 		switch ($strage[CODE]) {
-			case READ:
+			case NEXT:
 				# code...
 				break;
-			case CREATE:
+			case PREV:
 				# code...
 				break;
-			case UPDATA:
-				# code...
-				break;
-			case DELETE:
-				# code...
-				break;
-			
+
 			default:
 				# code...
 				break;
@@ -42,26 +33,5 @@ class  extends CommonController {
 	public function step03() {
 		// read on file
 		require_once ('/Applications/MAMP/htdocs/PHP/PlainProject/plane.html');
-	}
-
-	// [MODEL][ITEM]...
-	public function setViewParam($param) {
-		$this->view_param = $param;
-	}
-
-	public function blankForm($type, $step) {
-		$document;
-		// // Creating an input item
-		// $document[DOMAIN][$type][$step] = [
-		// 	REPORT => '',
-		// 	ITEM   => '',
-		// 	RESULT => '',
-		// ];
-		//$document[USER]               = $this->strage[USER][DOMAIN][$type];
-		//$document[USER]               = $this->strage[USER][MODEL][$type];
-		$document[SYSTEM][CODE][STEP] = $step;
-		$document[SYSTEM][CODE][TYPE] = $type;
-		//$this->strage[SYSTEM][$type];
-		return $document;
 	}
 }
